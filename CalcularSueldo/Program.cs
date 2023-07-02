bccef9c7418057027a190a29a5a3449d2a31cc6f
andradajoaquin22 <andradajoaquin22@gmail.com>
2023-07-02 20:23:03 -0300

    p

---
+++ CalcularSueldo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularSueldo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horasTrabajadas;
+            float costoHora;
+            float sueldo;
+            string linea;
+            Console.Write("ingrese horas trabajadas por el operario:");
+            linea= Console.ReadLine();
+            horasTrabajadas=int.Parse(linea);
+            Console.Write("Ingrese el pago por hora:");
+            linea = Console.ReadLine();
+            costoHora = float.Parse(linea);
+            sueldo = horasTrabajadas * costoHora;
+            Console.Write("El sueldo total del operario es: ");
+            Console.Write(sueldo);
+            Console.ReadKey();
+
+        }
+    }
+}
